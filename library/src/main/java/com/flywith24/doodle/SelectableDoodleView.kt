f5c33e72ddab0e3910ebe079c1a2cfb4e4cc998b
fly_with24 <fly_with24@163.com>
2020-08-03 15:23:24 +0800

    add bound

--- library/src/main/java/com/flywith24/doodle/SelectableDoodleView.kt
+++ library/src/main/java/com/flywith24/doodle/SelectableDoodleView.kt
@@ -20,12 +20,19 @@ class SelectableDoodleView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr), GestureDetector.OnGestureListener {
     private val mPaint by lazy {
-        Paint().apply {
+        Paint(Paint.ANTI_ALIAS_FLAG).apply {
             color = Color.RED
             style = Paint.Style.STROKE
             strokeWidth = 20f
-            isAntiAlias = true
             strokeCap = Paint.Cap.ROUND
+        }
+    }
+    private val mBoundPaint by lazy {
+        Paint(Paint.ANTI_ALIAS_FLAG).apply {
+            color = Color.WHITE
+            style = Paint.Style.STROKE
+            strokeWidth = STROKE_WIDTH
+            strokeCap = Paint.Cap.SQUARE
         }
     }
     private val mGestureDetector by lazy { GestureDetector(context, this) }
@@ -39,8 +46,11 @@ class SelectableDoodleView @JvmOverloads constructor(
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
         if (event.actionMasked == MotionEvent.ACTION_UP) {
-            // 置空 path，代表一次涂鸦完成
-            mCurrentPath = null
+            if (mSelectedPath == null) {
+                // 置空 path，代表一次涂鸦完成
+                mCurrentPath = null
+            }
+
             Log.i(TAG, "onTouchEvent: ACTION_UP")
         }
         return mGestureDetector.onTouchEvent(event)
@@ -49,9 +59,18 @@ class SelectableDoodleView @JvmOverloads constructor(
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         for (item in mPathList) {
+            val bounds = item.bounds
+            if (mSelectedPath == item) {
+                canvas.drawRect(
+                    bounds.left - INSIDE_WIDTH,
+                    bounds.top - INSIDE_WIDTH,
+                    bounds.right + INSIDE_WIDTH,
+                    bounds.bottom + INSIDE_WIDTH,
+                    mBoundPaint
+                )
+            }
             canvas.save()
             canvas.translate(item.offsetX, item.offsetY)
-            mPaint.color = if (mSelectedPath == item) Color.YELLOW else Color.RED
             canvas.drawPath(item.path, mPaint)
             canvas.restore()
         }
@@ -69,18 +88,18 @@ class SelectableDoodleView @JvmOverloads constructor(
     }
 
     private fun isInPath(e: MotionEvent): PathItem? {
-        val bounds = RectF()
         for (item in mPathList) {
-            item.path.computeBounds(bounds, true)
-            bounds.offset(item.offsetX, item.offsetY)
-            if (bounds.contains(e.x, e.y)) {
+            if (item.bounds.contains(e.x, e.y)) {
                 return item
             }
         }
         return null
     }
 
-    override fun onDown(e: MotionEvent?): Boolean {
+    override fun onDown(e: MotionEvent): Boolean {
+        Log.i(TAG, "onDown: ")
+        if (mSelectedPath != null && isInPath(e) == null) mSelectedPath = null
+
         return true
 
     }
@@ -102,19 +121,30 @@ class SelectableDoodleView @JvmOverloads constructor(
         distanceX: Float,
         distanceY: Float
     ): Boolean {
-        if (mCurrentPath == null) {
-            //第一笔，设置起点
-            mCurrentPath = PathItem()
-            mPathList.add(mCurrentPath!!)
-            mCurrentPath!!.path.moveTo(e1.x, e1.y)
-            mLastX = e1.x
-            mLastY = e1.y
+        if (mSelectedPath == null) {
+            if (mCurrentPath == null) {
+                //第一笔，设置起点
+                mCurrentPath = PathItem()
+                mPathList.add(mCurrentPath!!)
+                mCurrentPath!!.path.moveTo(e1.x, e1.y)
+                mLastX = e1.x
+                mLastY = e1.y
+            } else {
+                //贝塞尔曲线
+                mCurrentPath!!.path.quadTo(
+                    mLastX,
+                    mLastY,
+                    (e2.x + mLastX) / 2f,
+                    (e2.y + mLastY) / 2f
+                )
+                mLastX = e2.x
+                mLastY = e2.y
+            }
         } else {
-            //贝塞尔曲线
-            mCurrentPath!!.path.quadTo(mLastX, mLastY, (e2.x + mLastX) / 2f, (e2.y + mLastY) / 2f)
-            mLastX = e2.x
-            mLastY = e2.y
+            mSelectedPath!!.offsetX = mSelectedPath!!.offsetX - distanceX
+            mSelectedPath!!.offsetY = mSelectedPath!!.offsetY - distanceY
         }
+
         invalidate()
         return false
     }
@@ -125,11 +155,32 @@ class SelectableDoodleView @JvmOverloads constructor(
 
     class PathItem {
         val path = Path()
+        val bounds: RectF = RectF()
+            get() {
+                path.computeBounds(field, true)
+                field.offset(offsetX, offsetY)
+                return field
+            }
         var offsetX = 0f
         var offsetY = 0f
     }
 
     companion object {
         private const val TAG = "SelectableDoodleView"
+
+        /**
+         * 选中框宽度
+         */
+        private val STROKE_WIDTH = 2f.dp
+
+        /**
+         * 选中框内边距
+         */
+        private val INSIDE_WIDTH = 5 * STROKE_WIDTH
+
+        /**
+         * 防止滚出的安全距离
+         */
+        private val SAFE_WIDTH = 10f.dp
     }
 }
